ab3953161a27c7f96775d8296195bb04efe652ce
Vanessa <vanessa.mercado24@gmail.com>
2022-07-07 16:52:34 -0700

    remove un used props

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,6 @@ const ButtonStyle = `
 bg-gradient-200 hover:bg-gradient-250 text-white py-2 px-4 rounded shadow
 `
 
-interface HomeProps {
-  currentAccount: string;
-};
-
-
 const Home: NextPage = () => {
   const [currentAccount, setCurrentAccount] = useState<string>('');
 
