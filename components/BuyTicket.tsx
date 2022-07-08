083e4066bf87e8a6cad85c29ac86ec99b47dc3de
Vanessa <vanessa.mercado24@gmail.com>
2022-07-08 06:57:40 -0700

    wip

--- components/BuyTicket.tsx
+++ components/BuyTicket.tsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react"
-// import { TicketSVG } from './TicketSVG'
+import { TicketSVG } from './TicketSVG'
+
+const ButtonStyle = `
+bg-gradient-200
+hover:bg-gradient-250
+text-white
+py-2
+px-4
+rounded
+shadow
+self-center
+w-1/5
+`
 
 export default function BuyTicket() {
-    // const [stringToDisplay, setStringToDisplay] = useState('')
+    const [stringToDisplay, setStringToDisplay] = useState<string>('100')
 
-    const ButtonStyle = `
-    bg-gradient-200
-    hover:bg-gradient-250
-    text-white
-    py-2
-    px-4
-    rounded
-    shadow
-    self-center
-    w-1/5
-    `
 
     const CONTRACT_ADDRESS = '0xd7573740BeFa52E4903B28dB0A7c12d810dc6e4C'
 
@@ -25,7 +26,7 @@ export default function BuyTicket() {
     return (
         <>
             <div className='flex self-start flex-col'>
-                {/* <TicketSVG stringToDisplay={'test'} /> */}
+                <TicketSVG stringToDisplay={stringToDisplay} />
                 <button className={ButtonStyle} onClick={mintTicket}>Mint Ticket</button>
             </div>
         </>
