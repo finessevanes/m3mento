5b086b2ee3fc26b645fcd8d287cd3da18e1971c0
Vanessa <vanessa.mercado24@gmail.com>
2022-07-28 08:05:54 -0700

    wip

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,8 +1,43 @@
+import { useState, useEffect } from "react"
+import { useAccount, useProvider, useContract } from 'wagmi'
+const CONTRACT_ADDRESS = '0x48ec8e99054527074d426a6d590e6c784e21757D'
+import contractInterface from '../src/utils/mintNFT.json'
+
 function Admin() {
-    return(
-        <>
-            <h1>Admin page</h1>
-        </>
+    const [isSaleOpen, setIsSaleOpen] = useState<boolean>(false)
+    const ButtonStyle = `
+    bg-gradient-200 hover:bg-gradient-250 text-white py-2 px-4 rounded shadow
+    `
+
+    const provider = useProvider()
+
+    const contractProvider = useContract({
+      addressOrName: CONTRACT_ADDRESS,
+      contractInterface: contractInterface.abi,
+      signerOrProvider: provider,
+    })
+
+    useEffect(() => {
+        const checkSale = async () => {
+            if (!contractProvider) return;
+
+            const isSaleOpen = await contractProvider.isSaleOpen()
+            setIsSaleOpen(isSaleOpen)
+        }
+
+        checkSale()
+        console.log(isSaleOpen)
+    }, [contractProvider, isSaleOpen])
+    return (
+        <div className="flex justify-center mt-60">
+            <div className="bg-red-100 rounded-lg flex flex-col p-12 text-center">
+                <h1 className="">Admin Panel</h1>
+                <input />
+                <button className={ButtonStyle}>Check In</button>
+                <button className={ButtonStyle}>Open Sale</button>
+                <button className={ButtonStyle}>Close Sale</button>
+            </div>
+        </div>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,58 @@ import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import contractInterface from '../src/utils/mintNFT.json'
+import Link from 'next/link';
 
-
-import { useAccount } from 'wagmi'
+import { useAccount, useProvider, useContract } from 'wagmi'
 import Layout from '../src/components/Layout'
 import EventsCarousel from '../src/components/EventsCarousel'
 import Navbar from '../src/components/Navbar'
 
+const CONTRACT_ADDRESS = '0x48ec8e99054527074d426a6d590e6c784e21757D'
+
+
 const Home = () => {
+  const [isAdmin, setIsAdmin] = useState<boolean>(false)
   const { address } = useAccount()
 
+  const provider = useProvider()
+
+  const contractProvider = useContract({
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: contractInterface.abi,
+    signerOrProvider: provider,
+  })
+
+
+  const checkIfAdmin = async () => {
+    if (!contractProvider) return;
+    try {
+      const ownerOfContract = await contractProvider.owner()
+      if (address === ownerOfContract) {
+        setIsAdmin(true)
+      } else {
+        setIsAdmin(false)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    checkIfAdmin()
+  })
   return (
     <div className=''>
       <div className="bg-hero-cover bg-cover">
-        <div className='flex justify-space-between top-0 sticky z-40'>
-        <button className='bg-red-200'>Admin</button>
+        <div className={`flex top-0 sticky z-40 justify-between`}>
+          {isAdmin &&
+              (<Link href='/admin'>
+                <a>
+                  Admin
+                </a>
+              </Link>)
+          }
           <div className='mt-3 mr-3'><ConnectButton /></div>
         </div>
         <div className="object-fill h-screen z-20 w-full flex justify-center items-end">
