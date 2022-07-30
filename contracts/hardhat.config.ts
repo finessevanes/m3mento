0f79024dff84820a778a2b03139e79046c9b8ae7
Vanessa <vanessa.mercado24@gmail.com>
2022-07-30 15:44:10 -0700

    udpated config file

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,12 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.DURBAN_POISON_PRIVATE_KEY || ''
+const ALCHEMY_KEY_RINKEBY = process.env.ALCHEMY_KEY_RINKEBY || ''
 
 const config: HardhatUserConfig = {
   solidity: "0.8.1",
   networks: {
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY_RINKEBY}`,
-      accounts: ['0000000000000']
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY_RINKEBY}`,
+      accounts: [PRIVATE_KEY]
     },
   }
 };
---
+++ pages/my-events.tsx
@@ -0,0 +1,8 @@
+function MyEvents() {
+
+    return (
+        <h1 className="font-white">MyEvents</h1>
+    )
+}
+
+export default MyEvents
