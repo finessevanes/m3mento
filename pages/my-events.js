04bdd8fe31a98ab217f386417f17611ca4db90d5
Vanessa <vanessa.mercado24@gmail.com>
2022-08-01 22:01:59 -0700

    user can view tickets // does not update when user logs out

---
+++ pages/my-events.js
@@ -0,0 +1,115 @@
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { useAccount, useProvider, useContract, useSigner } from 'wagmi'
+import contractInterface from '../src/utils/abi.json'
+import { ConnectButton, connectorsForWallets } from '@rainbow-me/rainbowkit';
+
+const MyEventss = () => {
+    const CONTRACT_ADDRESS = '0x62F2492668f40e699F6B20D9db89cB173a58031F'
+    const [isUserCheckedIn, setIsUserCheckedIn] = useState(false)
+    const [addressSignedIn, setAddressSignedIn] = useState('')
+    const [tickets, setTickets] = useState([])
+
+    console.log('hello', tickets)
+
+
+    const ButtonStyle = `
+    bg-gradient-200 hover:bg-gradient-250 text-white py-2 px-4 rounded shadow mt-2 disabled:bg-gradient-100
+    `
+
+    const imageStyle = {
+        borderRadius: '12px',
+        margin: '0'
+    };
+
+    const provider = useProvider()
+    const { address } = useAccount()
+    const { data: signer, isError, isLoading } = useSigner()
+
+    const contractSigner = useContract({
+        addressOrName: CONTRACT_ADDRESS,
+        contractInterface: contractInterface.abi,
+        signerOrProvider: signer,
+    })
+
+    const contractProvider = useContract({
+        addressOrName: CONTRACT_ADDRESS,
+        contractInterface: contractInterface.abi,
+        signerOrProvider: provider,
+    })
+
+    const handleCheckIn = async () => {
+        if (!addressSignedIn) return;
+        try {
+            console.log('address to be checked in: ', addressSignedIn)
+            console.log('... checking in')
+            let checkInTxn = await contractSigner.checkIn(addressSignedIn)
+            await checkInTxn.wait()
+            setIsUserCheckedIn(true)
+            console.log('complete!')
+            console.log(`TRANSACTION: https://rinkeby.etherscan.io/tx/${checkInTxn.hash}`)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+
+    useEffect(() => {
+        if (!addressSignedIn) return
+        const checkIfUserHoldsTicket = async () => {
+            try {
+                let confirmOwnership = await contractProvider.getConfirmOwnership(addressSignedIn)
+                console.log('confirmOwnership: ', confirmOwnership)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        checkIfUserHoldsTicket()
+    })
+
+    useEffect(() => {
+        if (!address) return
+        const checkAddress = async () => {
+            setAddressSignedIn(address)
+        }
+
+        checkAddress()
+    }, [address])
+
+    useEffect(() => {
+        if (!address) return
+        console.log(address)
+        fetch(`https://rinkeby-api.opensea.io/api/v1/assets?owner=${address}&asset_contract_address=${CONTRACT_ADDRESS}`)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.assets?.length) {
+                    setTickets(data.assets)
+                }
+            })
+            .catch(e => console.log(e))
+    }, [address])
+
+
+    return (
+        <>
+            <ConnectButton />
+            <div className="flex justify-center mt-60">
+                <div className="rounded-lg flex flex-col p-12 text-center">
+                    {tickets.length > 0 && address && tickets.map(ticket => (
+                        <>
+                            <h1 className='text-white font-semibold text-xl mb-2'>{ticket.name}</h1>
+                            <h2 className='text-white font-semibold mb-2'>{ticket.description}</h2>
+                            <Image src={ticket.image_original_url} width={300} height={400} alt={ticket.description} style={imageStyle} />
+                            <button disabled={isUserCheckedIn} className={ButtonStyle} onClick={handleCheckIn}>Check In</button>
+                        </>
+                    ))}
+                    {!tickets.length && (
+                        <h1 className='text-white font-semibold text-xl mb-2'>You dont have tickets 🥲</h1>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MyEventss
