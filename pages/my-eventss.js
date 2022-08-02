ef9b2abc5491c8cb9ea46fbe1ca37fcfe46c06d7
Vanessa <vanessa.mercado24@gmail.com>
2022-08-01 19:52:02 -0700

    add js my-eventss

---
+++ pages/my-eventss.js
@@ -0,0 +1,107 @@
+import { useEffect, useState } from 'react'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import { useAccount, useProvider, useContract, useSigner } from 'wagmi'
+
+import contractInterface from '../src/utils/abi.json'
+
+const MyEvents = ({ data }) => {
+    const CONTRACT_ADDRESS = '0x62F2492668f40e699F6B20D9db89cB173a58031F'
+    const [isUserCheckedIn, setIsUserCheckedIn] = useState(false)
+    const [addressSignedIn, setAddressSignedIn] = useState('')
+
+
+    const ButtonStyle = `
+    bg-gradient-200 hover:bg-gradient-250 text-white py-2 px-4 rounded shadow mt-2 disabled:bg-gradient-100
+    `
+
+    const provider = useProvider()
+    const { address } = useAccount()
+    const { data: signer, isError, isLoading } = useSigner()
+
+    const contractSigner = useContract({
+        addressOrName: CONTRACT_ADDRESS,
+        contractInterface: contractInterface.abi,
+        signerOrProvider: signer,
+    })
+
+    const contractProvider = useContract({
+        addressOrName: CONTRACT_ADDRESS,
+        contractInterface: contractInterface.abi,
+        signerOrProvider: provider,
+    })
+
+    const handleCheckIn = async () => {
+        if (!address) return;
+        try {
+            console.log('address to be checked in: ', address)
+            console.log('... checking in')
+            let checkInTxn = await contractSigner.checkIn(address)
+            await checkInTxn.wait()
+            setIsUserCheckedIn(true)
+            console.log('complete!')
+            console.log(`TRANSACTION: https://rinkeby.etherscan.io/tx/${checkInTxn.hash}`)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const checkIfUserHoldsTicket = async (addy) => {
+        try {
+            let confirmOwnership = await contractProvider.getConfirmOwnership(addy)
+            console.log('confirmOwnership: ', confirmOwnership)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        if (address) {
+            checkIfUserHoldsTicket(address)
+        }
+    })
+
+    useEffect(() => {
+        if (!address) return
+        const checkAddress = async () => {
+            setAddressSignedIn(address)
+            console.log('address', address)
+        }
+
+        checkAddress()
+    }, [address])
+
+    useEffect(() => {
+        fetch(`https://rinkeby-api.opensea.io/api/v1/assets?owner=${address}&asset_contract_address=${CONTRACT_ADDRESS}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const { image_original_url: imageOriginalUrl, name, token_id: tokenId } = data?.assets[0]
+                console.log('image_original_url', imageOriginalUrl)
+                console.log('name', name)
+                console.log('tokenId', tokenId)
+            });
+    })
+
+
+    return (
+        <div className="flex justify-center mt-60">
+            <div className="bg-red-100 rounded-lg flex flex-col p-12 text-center">
+                <h1 className="">My Events</h1>
+                <h1>Nights and Weekends</h1>
+                <button disabled={isUserCheckedIn} className={ButtonStyle} onClick={handleCheckIn}>Check In</button>
+            </div>
+        </div>
+    )
+}
+
+//   export const getServerSideProps: GetServerSideProps = async (context) => {
+//     // Fetch data from external API
+//     fetch(`https://rinkeby-api.opensea.io/api/v1/assets?owner=${address}&asset_contract_address=${CONTRACT_ADDRESS}`)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.assets[0])); 
+
+//     // Pass data to the page via props
+//     return { props: { data } }
+//   }
+
+
+export default MyEvents
